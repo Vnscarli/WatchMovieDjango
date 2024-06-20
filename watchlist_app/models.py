db1dc7cb73268bef32be07dc3d3171ef82fd36df
Vnscarli <vnsmesdes7@gmail.com>
2024-06-20 14:56:32 -0300

    Add Json responde for movies

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    active = models.BooleanField(default=True)
+    ageRestriction = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.name
