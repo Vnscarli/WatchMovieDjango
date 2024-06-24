8a442e0d210a846a7baae9f8f43fbd835010f0f5
Vnscarli <vnsmesdes7@gmail.com>
2024-06-24 17:43:43 -0300

    Add Validation to Serializers

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
+def name_length(value):
+    if len(value) < 2:
+        raise serializers.ValidationError("Name must have more than 2 letters")
+    elif len(value)>50:
+        raise serializers.ValidationError("Name must have less than 50 letters")
+    return value
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=50)
+    name = serializers.CharField(validators=[name_length])
     description = serializers.CharField(max_length=500)
     active = serializers.BooleanField()
     ageRestriction = serializers.IntegerField()
@@ -18,4 +25,16 @@ class MovieSerializer(serializers.Serializer):
         instance.ageRestriction = validated_data.get('ageRestriction', instance.ageRestriction)
         instance.save()
         return instance
+    
+    def validate(self, data):
+        if data['name']==data['description']:
+            raise serializers.ValidationError("Name must not be equal to description")
+        return data
+    
+    
+    def validate_ageRestriction(self, value):
+        if value!=0 and value!=10 and value!=12 and value!=14 and value!=16 and value!=18:
+            raise serializers.ValidationError("Age restriction not possible") 
+        return value
+    
     
