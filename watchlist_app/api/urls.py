d390a3e362e15866c4cfd21825f03ed48168912c
Vnscarli <vnsmesdes7@gmail.com>
2024-07-13 15:14:29 -0300

    Add GenericViews and list reviews per movie in urls

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 #from watchlist_app.api.views import movie_list, movie_info
-from watchlist_app.api.views import MovieListAV, MovieInfoAV, StreamingPlatformListAV, StremingPlatformInfoAV, ReviewsInfoAV, ReviewsListAV
+from watchlist_app.api.views import (MovieListAV, MovieInfoAV, StreamingPlatformListAV, ReviewsCreate,
+                                     StremingPlatformInfoAV, ReviewsInfo, ReviewsList)
 
 urlpatterns=[
     path('movies/', MovieListAV.as_view(), name='movie-list'),
     path('movies/<int:pk>/', MovieInfoAV.as_view(), name='movie-detail'),
+    
     path('platform/', StreamingPlatformListAV.as_view(), name='platform-list'),
     path('platform/<int:pk>', StremingPlatformInfoAV.as_view(), name='streamingplatform-detail'),
-    path('reviews/', ReviewsListAV.as_view(), name='reviews-list'),
-    path('reviews/<int:pk>', ReviewsInfoAV.as_view(), name='reviews-detail')
+    
+    path('platform/<int:pk>/reviewscreate', ReviewsCreate.as_view(), name='reviews-create'),
+    path('reviews/<int:pk>', ReviewsInfo.as_view(), name='reviews-detail'),
+    path('platform/<int:pk>/reviews', ReviewsList.as_view(), name='reviews-list')
 ]
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -7,35 +7,27 @@ from watchlist_app.models import Movie, StreamingPlatform, Review
 from  watchlist_app.api.serializers import MovieSerializer, StreamingPlatformSerializer, ReviewSerializer
 
 
-class ReviewsListAV(mixins.ListModelMixin,
-                mixins.CreateModelMixin,
-                generics.GenericAPIView):
+class ReviewsCreate(generics.CreateAPIView):
+    serializer_class=ReviewSerializer
+    
+    def perform_create(self, serializer):
+        pk = self.kwargs.get('pk')
+        movie = Movie.objects.get(pk=pk)
+        
+        serializer.save(movies=movie)
+
+class ReviewsList(generics.ListAPIView):
+    serializer_class=ReviewSerializer
+    
+    
+    def get_queryset(self):
+        pk = self.kwargs['pk']
+        return Review.objects.filter(movies=pk)
+    
+class ReviewsInfo(generics.RetrieveUpdateDestroyAPIView):
     queryset=Review.objects.all()
     serializer_class=ReviewSerializer
     
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-    
-
-class ReviewsInfoAV(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics. GenericAPIView):
-    queryset=Review.objects.all()
-    serializer_class=ReviewSerializer
-    
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
 class StreamingPlatformListAV(mixins.ListModelMixin,
                               mixins.CreateModelMixin,
                               generics.GenericAPIView):
@@ -61,28 +53,21 @@ class StreamingPlatformListAV(mixins.ListModelMixin,
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) """
     
-class StremingPlatformInfoAV(APIView):
+class StremingPlatformInfoAV(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics. GenericAPIView):
+    queryset= StreamingPlatform.objects.all()
+    serializer_class= StreamingPlatformSerializer
     
-    def get(self, request, pk):
-        try:
-            platform = StreamingPlatform.objects.get(pk=pk)
-        except StreamingPlatform.DoesNotExist:
-            return Response({'error': 'Streaming Platform not found'}, status=status.HTTP_404_NOT_FOUND)
-        serializer= StreamingPlatformSerializer(platform, context={'request':request})
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
     
-    def put(self, request, pk):
-        platform = StreamingPlatform.objects.get(pk=pk)
-        serializer= StreamingPlatformSerializer(platform, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
     
-    def delete(self, request, pk):
-        platform=StreamingPlatform.objects.get(pk=pk)
-        platform.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
 class MovieListAV (APIView):
     
